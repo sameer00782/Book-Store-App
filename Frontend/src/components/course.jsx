7bad5e101c68aab895b8830240498e9a226b7678
Sameer Sonwane <sameersonwane08@gmail.com>
2024-09-27 23:10:28 +0530

    new changes

--- Frontend/src/components/course.jsx
+++ Frontend/src/components/course.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import Cards from '../components/cards.jsx';
+import Cards from './cards.jsx';
 import axios from "axios";
 import { Link } from 'react-router-dom';
 
