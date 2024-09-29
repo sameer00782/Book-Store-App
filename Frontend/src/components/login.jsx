8c594dbb2d14480db664fd92fe61e66fc3aaa469
Sameer Sonwane <sameersonwane08@gmail.com>
2024-09-29 12:27:07 +0530

    new changes

--- Frontend/src/components/login.jsx
+++ Frontend/src/components/login.jsx
@@ -18,7 +18,7 @@ function Login() {
             email: data.email,
             password: data.password,
         };
-       await axios.post("http://localhost:4001/users/login", userInfo)
+       await axios.post("https://book-store-app-backend-sand.vercel.app/users/login", userInfo)
         .then((res)=>{
             console.log(res.data)
             if (res.data){
