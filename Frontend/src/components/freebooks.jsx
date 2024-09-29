a9be1d465ba4f44afc25b5e9d8808731f6fbd4af
Sameer Sonwane <sameersonwane08@gmail.com>
2024-09-29 19:11:40 +0530

    new changes

--- Frontend/src/components/freebooks.jsx
+++ Frontend/src/components/freebooks.jsx
@@ -11,7 +11,7 @@ function FreeBooks() {
   useEffect(() => {
     const getBook = async () => {
       try {
-        const res = await axios.get("https://book-store-app-backend-p27r.onrender.com/book");
+        const res = await axios.get("http://localhost:4001/book");
         console.log(res.data);
         const data = res.data.filter((data) => data.category === "free");
         setBook(data); 
